f5f8d08b32b38f7c980e6c2789eb53fc5866108f
Mohammed chaouki <medchaouki12@icloud.com>
2025-02-28 20:16:03 +0800

    added some more impltns

--- Cat.java
+++ Cat.java
@@ -3,32 +3,40 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Cat {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         if (args.length > 0) {
 
             try {
 
-                File file = new File(args[0]);
-                FileReader fR = new FileReader(file);
-                BufferedReader reader = new BufferedReader(fR);
+                for (String op : args) {
+                    File file = new File(args[0]);
+                    FileReader fR = new FileReader(file);
+                    BufferedReader reader = new BufferedReader(fR);
 
-                String line;
+                    String line;
 
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+
                 }
-
             } catch (FileNotFoundException e) {
                 System.out.println(e.getMessage());
 
             } catch (IOException ex) {
                 System.out.println(ex.getMessage());
             }
-        } else {
-            System.out.println("No argument passed");
+        }
+
+        if (args.length == 0) {
+            String typed = scanner.nextLine();
+            System.out.println(typed);
         }
     }
 }
